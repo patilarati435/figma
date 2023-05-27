7721b46c569f7b22459aa4a74618b1146650fe12
karuna123761 <karuna22parihar@gmail.com>
2023-05-27 12:30:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1>Axiom Techguru Pvt </h1>
+    </div>
+  )
+}
+
+export default App
